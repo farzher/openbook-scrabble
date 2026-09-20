93b3344d6488be6e11fbbd690bbf43971ab79aec
ヽ◕◡◕✿ノ <stephenkamenar@gmail.com>
2026-09-20 06:58:29 -0400

    Load progressive EV scheduler

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import Serverless_Lobby from 'https://farzher.com/assets/serverless_lobby.js'
 import {SIZE, PREMIUM, LETTER_SCORES, DISTRIBUTION, Lexicon, generateMoves, createGame, publicState, processAction, keyOfMove, checkStandardTimeout, normalizeTimerConfig} from './game.js'
 
 import {sound, toggleSound, soundEnabled, unlockAudio} from './sounds.js'
-import {initThreats,updateThreats,prefetchThreats} from './threat-ui.js?v=ev-prefetch1'
+import {initThreats,updateThreats,prefetchThreats} from './threat-ui.js?v=ev-progressive1'
 
 const DICTIONARY_URL='https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt'
 const DIRECTORY_CHANNEL='openbook-scrabble:directory'
